19aaf81c1d564b2bc34f8b49364e0bff1e4b94b0
skumar <kumasachin@gmail.com>
2025-07-04 01:48:51 +0100

    varius fixes:

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
 import type { AxiosInstance } from "axios";
 
-interface ImportMetaEnv {
-  MODE: "development" | "production";
-}
-
 const baseURL: string =
   import.meta.env.MODE === "development" ? "http://localhost:4001/api" : "/api";
 
--- frontend/src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function formatMessageTime(date: string | number | Date): string {
-  const parsedDate = new Date(date);
-  return parsedDate.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-  });
-}
